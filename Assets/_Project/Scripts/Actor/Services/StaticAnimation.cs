4fc8efe004950ace83744f94a779f08f64bc4ce1
ParanoidMann <Paranoid.Mann.21@gmail.com>
2022-10-18 21:31:16 +0300

    Static Npc Animations

---
+++ Assets/_Project/Scripts/Actor/Services/StaticAnimation.cs
@@ -0,0 +1,32 @@
+﻿using ParanoidMann.Core;
+
+using UnityEngine;
+
+namespace ParanoidMann.Affluenza.Actor
+{
+	[RequireComponent(typeof(Animator))]
+	public class StaticAnimation : MonoBehaviour
+	{
+		[SerializeField]
+		private Animator _animator;
+		[SerializeField]
+		private string _clipTrigger;
+
+		private void Start()
+		{
+			if (!_clipTrigger.IsNullOrEmptyOrWhiteSpace())
+			{
+				int hash = Animator.StringToHash(_clipTrigger);
+				_animator.SetTrigger(hash);
+			}
+		}
+
+		private void OnValidate()
+		{
+			if (_animator == null)
+			{
+				_animator = GetComponent<Animator>();
+			}
+		}
+	}
+}
